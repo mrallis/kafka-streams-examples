73dbd865e65f3b9911ee274f6a5187a2533d8cdb
Michail Rallis <mrallis@Michails-MacBook-Pro.local>
2022-04-12 14:37:58 +0100

    Adds validation aggregator logic

--- src/main/java/io/confluent/examples/streams/microservices/exercises/ValidationsAggregatorService.java
+++ src/main/java/io/confluent/examples/streams/microservices/exercises/ValidationsAggregatorService.java
@@ -106,10 +106,7 @@ public class ValidationsAggregatorService implements Service {
     //If all rules pass then validate the order
     validations
         .groupByKey(serdes3)
-
-        // TODO 5.1: window the data using `KGroupedStream#windowedBy`, specifically using `SessionWindows.with` to define 5-minute windows
-        // ...
-
+        .windowedBy(SessionWindows.ofInactivityGapWithNoGrace(Duration.ofMinutes(5)))
         .aggregate(
             () -> 0L,
             (id, result, total) -> PASS.equals(result.getValidationResult()) ? total + 1 : total,
@@ -137,13 +134,8 @@ public class ValidationsAggregatorService implements Service {
                 newBuilder(order).setState(OrderState.FAILED).build(),
             JoinWindows.of(Duration.ofMinutes(5)), serdes7)
         //there could be multiple failed rules for each order so collapse to a single order
-
-        // TODO 5.2: group the records by key using `KStream#groupByKey`, providing the existing Serialized instance for ORDERS
-        // ...
-
-        // TODO 5.3: use an aggregation operator `KTable#reduce` to collapse the records in this stream to a single order for a given key
-        // ...
-
+            .groupByKey(serdes6)
+            .reduce((order1, order2) -> order1)
         //Push the validated order into the orders topic
         .toStream().to(ORDERS.name(), Produced.with(ORDERS.keySerde(), ORDERS.valueSerde()));
 
